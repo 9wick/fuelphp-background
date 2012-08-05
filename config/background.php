735986c5b4dff53d3a35f474eb8e3b7004e65591
木戸 康平 <bouningenn@gmail.com>
2012-08-05 20:50:10 +0900

    fix spaces

--- config/background.php
+++ config/background.php
@@ -1,29 +1,23 @@
 <?php
 
 return array(
-
-	/**
-	 * Default settings
-	 */
-	'defaults' => array(
-        
+    /**
+     * Default settings
+     */
+    'defaults' => array(
         /**
          * Path of PHP (ex /usr/local/bin/php, c:\bin\php or php ( when system path is already set) )
          */
         'php_path' => 'php',
-		
-	),
-
-	/**
-	 * Default setup group
-	 */
-	'default_setup' => 'default',
-
-	/**
-	 * Setup groups
-	 */
-	'setups' => array(
-		'default' => array(),
-	),
-
+    ),
+    /**
+     * Default setup group
+     */
+    'default_setup' => 'default',
+    /**
+     * Setup groups
+     */
+    'setups' => array(
+        'default' => array(),
+    ),
 );
